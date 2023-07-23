7b4503c57a18dab0514037ef512656d4c4f9ea42
penpeen <ikegami1022.h@gmail.com>
2023-07-24 00:02:14 +0900

    :recycle: rubocopの適用

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,13 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
-      g.test_framework :rspec,
+      g.test_framework :rspec, {
         fixture: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
         request_specs: false
+      }
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
