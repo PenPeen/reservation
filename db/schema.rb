2dea7da534bba44ddeba1aaa644bed6b290b3f95
penpeen <ikegami1022.h@gmail.com>
2023-07-24 21:34:38 +0900

    :sparkles: Resourceモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_140531) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_123220) do
   create_table "identities", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -29,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_140531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["identity_id"], name: "index_reservation_general_roles_on_identity_id"
+  end
+
+  create_table "resources", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", limit: 50
+    t.string "note", limit: 1024
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "reservation_admin_roles", "identities"
