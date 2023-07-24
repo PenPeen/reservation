6180d5e1a214cdd3707635e51e0be22e12f3f565
penpeen <ikegami1022.h@gmail.com>
2023-07-24 21:42:15 +0900

    :sparkles: Reservationモデルの実装

---
+++ spec/factories/reservations.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :reservation do
+    identity { nil }
+    resource { nil }
+    start_time { "2023-07-24 12:38:58" }
+    end_time { "2023-07-24 12:38:58" }
+  end
+end
