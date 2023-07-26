8d844cd8f4fa99abf92066023c1219deef2d4683
penpeen <ikegami1022.h@gmail.com>
2023-07-26 23:39:40 +0900

    :recycle: リファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,28 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		identity = Identity.find_by(email: params[:email])
-		if identity && identity.authenticate(params[:password])
-			if identity.reservation_admin_role
-				session[:reservation_admin] = identity.id
-				redirect_to admin_path
-			elsif identity.reservation_general_role
-				session[:reservation_general] = identity.id
-				redirect_to general_path
-			else
-				redirect_to login_path, alert: 'このIDにはロールが割り当てられていません。'
-			end
-		else
-			redirect_to login_path, alert: 'メールアドレスまたはパスワードが無効です。'
-		end
-	end
+  def create
+    identity = Identity.find_by(email: params[:email])
+    if identity&.authenticate(params[:password])
+      if identity.reservation_admin_role
+        session[:reservation_admin] = identity.id
+        redirect_to admin_path
+      elsif identity.reservation_general_role
+        session[:reservation_general] = identity.id
+        redirect_to general_path
+      else
+        redirect_to login_path, alert: 'このIDにはロールが割り当てられていません。'
+      end
+    else
+      redirect_to login_path, alert: 'メールアドレスまたはパスワードが無効です。'
+    end
+  end
 
-	def destroy
-		session.delete(:reservation_admin)
-		session.delete(:reservation_general)
-		redirect_to login_path
-	end
+  def destroy
+    session.delete(:reservation_admin)
+    session.delete(:reservation_general)
+    redirect_to login_path
+  end
 end
