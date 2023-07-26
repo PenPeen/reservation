1a75857fc7e9afbaef53922b66cd8813f401e2d0
penpeen <ikegami1022.h@gmail.com>
2023-07-26 23:29:00 +0900

    :memo: ログインログアウトの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
