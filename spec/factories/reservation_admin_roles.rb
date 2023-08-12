52863b226f9883e091ca5fa9fc723f96ae249105
penpeen <ikegami1022.h@gmail.com>
2023-08-12 10:55:44 +0900

    :memo: annotateの反映もれ

--- spec/factories/reservation_admin_roles.rb
+++ spec/factories/reservation_admin_roles.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: reservation_admin_roles
+#
+#  id          :bigint           not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  identity_id :bigint           not null
+#
+# Indexes
+#
+#  index_reservation_admin_roles_on_identity_id  (identity_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (identity_id => identities.id)
+#
 FactoryBot.define do
   factory :reservation_admin_role do
     association :identity, factory: :identity
--- spec/factories/reservation_general_roles.rb
+++ spec/factories/reservation_general_roles.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: reservation_general_roles
+#
+#  id          :bigint           not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  identity_id :bigint           not null
+#
+# Indexes
+#
+#  index_reservation_general_roles_on_identity_id  (identity_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (identity_id => identities.id)
+#
 FactoryBot.define do
   factory :reservation_general_role do
     association :identity, factory: :identity
