0006ad63cc2fa79058f9322afdae8c74107b1d03
penpeen <ikegami1022.h@gmail.com>
2023-08-13 15:38:51 +0900

    :sparkles: adminダッシュボード用のrouting定義

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get '/admin', to: 'admins#index'
+  namespace :admin do
+    root to: 'dashboards#index'
+  end
+
+  # OPTIMIZED: general機能実装時
   get '/general', to: 'generals#index'
 end
