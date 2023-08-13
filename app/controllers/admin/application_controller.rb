3aaf3627aa46c9a9fccc209ec67636ac85da5f68
penpeen <ikegami1022.h@gmail.com>
2023-08-13 16:43:00 +0900

    :sparkles: ログインしていないユーザーは管理者画面にアクセスできないように

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,16 @@
 module Admin
   class ApplicationController < ApplicationController
     layout 'admin'
+    before_action :authenticate_admin
+
+    private
+
+    def authenticate_admin
+      if session[:reservation_admin]
+        @current_user = Identity.find(session[:reservation_admin])
+      else
+        redirect_to login_path, alert: '管理者アカウントでログインしてください。'
+      end
+    end
   end
 end
